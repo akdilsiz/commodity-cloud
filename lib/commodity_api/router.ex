a45d88cd1c34a1ece9498b2b113034e2b0b650ff
Abdulkadir DILSIZ <akdilsiz@tecpor.com>
2019-03-29 20:03:31 +0300

    Added plugs on router pipeline

--- lib/commodity_api/router.ex
+++ lib/commodity_api/router.ex
@@ -16,26 +16,14 @@
 defmodule Commodity.Router do
   use Commodity.Api, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
+    plug CORSPlug, [origin: "*"]
     plug :accepts, ["json"]
+    plug Commodity.Api.Generic.TimeInformationPlug
+    plug Commodity.Api.Generic.Plug.PublicIp
   end
 
   scope "/", Commodity do
-    pipe_through :browser
-
-    get "/", PageController, :index
+    pipe_through :api
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Commodity do
-  #   pipe_through :api
-  # end
 end

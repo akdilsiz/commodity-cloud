da9db8a55926f5077fb8fb5552e1402ae1b48821
Abdulkadir DILSIZ <akdilsiz@tecpor.com>
2019-04-03 01:25:09 +0300

    Added public_ip module author url

--- lib/commodity_api/generic/plug/public_ip.ex
+++ lib/commodity_api/generic/plug/public_ip.ex
@@ -1,17 +1,5 @@
 ##
-#    Copyright 2018 Abdulkadir DILSIZ
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+#  https://www.cogini.com/blog/getting-the-client-public-ip-address-in-phoenix/
 ##   
 defmodule Commodity.Api.Generic.Plug.PublicIp do
 	@moduledoc """
@@ -33,7 +21,7 @@ defmodule Commodity.Api.Generic.Plug.PublicIp do
 	end
 
 	def process(conn, vals) do
-		if Application.get_env(:bisigortaci, :trust_x_forwarded_for, false) do
+		if Application.get_env(:commodity, :trust_x_forwarded_for, false) do
 			ip_address = get_ip_address(conn, vals)
 
 			# Rewrite standard remote_ip field with value from header
